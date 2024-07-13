faae137b4e0968236a14b6f6468d3bd323045926
BuildTools <unconfigured@null.spigotmc.org>
2024-07-13 17:58:40 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,104 @@
+
+const daily = document.querySelector("#daily")
+const weekly = document.querySelector("#weekly")
+const monthly = document.querySelector("#monthly")
+
+let setting = "daily"
+
+// Function to render the page
+function render(object) {
+
+    titles = document.querySelectorAll('.title')
+    time = document.querySelectorAll('.time')
+    prev = document.querySelectorAll('.previous-hours')
+    
+    switch(setting) {
+        case "daily":
+            daily.classList.add('selected')
+            weekly.classList.remove('selected')
+            monthly.classList.remove('selected')
+            for (i = 0; i < titles.length; i++) {
+                titles[i].innerText = object[i].title
+                if (object[i].timeframes.daily.current == 1) {
+                    time[i].innerText = object[i].timeframes.daily.current + "hr"
+                } else {
+                    time[i].innerText = object[i].timeframes.daily.current + "hrs"
+                }
+                
+                if (object[i].timeframes.daily.previous == 1) {
+                    prev[i].innerText = object[i].timeframes.daily.previous + "hr"
+                } else {
+                    prev[i].innerText = object[i].timeframes.daily.previous + "hrs"
+                }
+            };
+            break;
+
+        case "weekly":
+            daily.classList.remove('selected')
+            weekly.classList.add('selected')
+            monthly.classList.remove('selected')
+            for (i = 0; i < titles.length; i++) {
+                titles[i].innerText = object[i].title
+                if (object[i].timeframes.weekly.current == 1) {
+                    time[i].innerText = object[i].timeframes.weekly.current + "hr"
+                } else {
+                    time[i].innerText = object[i].timeframes.weekly.current + "hrs"
+                }
+                
+                if (object[i].timeframes.weekly.previous == 1) {
+                    prev[i].innerText = object[i].timeframes.weekly.previous + "hr"
+                } else {
+                    prev[i].innerText = object[i].timeframes.weekly.previous + "hrs"
+                }
+            };
+            break;
+
+        case "monthly":
+            daily.classList.remove('selected')
+            weekly.classList.remove('selected')
+            monthly.classList.add('selected')
+            for (i = 0; i < titles.length; i++) {
+                titles[i].innerText = object[i].title
+                if (object[i].timeframes.monthly.current == 1) {
+                    time[i].innerText = object[i].timeframes.monthly.current + "hr"
+                } else {
+                    time[i].innerText = object[i].timeframes.monthly.current + "hrs"
+                }
+                
+                if (object[i].timeframes.monthly.previous == 1) {
+                    prev[i].innerText = object[i].timeframes.monthly.previous + "hr"
+                } else {
+                    prev[i].innerText = object[i].timeframes.monthly.previous + "hrs"
+                }
+            };
+            break;
+    }
+}
+
+// Function to fetch the Json and then display it on screen
+function fetchJson() {
+    fetch('data.json')
+    .then(response => response.json())
+    .then(object => {
+        render(object)
+    })
+}
+
+// Initializes the page
+fetchJson()
+
+// Handles the list of buttons
+daily.addEventListener('click', function() {
+    setting = "daily"
+    fetchJson()
+})
+
+weekly.addEventListener('click', function() {
+    setting = "weekly"
+    fetchJson()
+})
+
+monthly.addEventListener('click', function() {
+    setting = "monthly"
+    fetchJson()
+})
